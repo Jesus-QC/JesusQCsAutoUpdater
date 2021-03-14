cc28bd8be9a42003547d679f1971ed5ab95e2ff6
jesus-qc <jesusquirocampos@gmail.com>
2021-03-14 13:30:09 +0100

    exiled.host api integration

--- AutoUpdater.cs
+++ AutoUpdater.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security;
 
 namespace JesusQCsAutoUpdater
 {
@@ -19,16 +20,30 @@ namespace JesusQCsAutoUpdater
         public override Version Version { get; } = new Version(1, 0, 3);
         public override PluginPriority Priority => PluginPriority.Lowest;
 
-        public Dictionary<string, Dictionary<string, string>> pluginList;
         public bool shouldSendDebug = true;
         public int updatedplugins = 0;
+        public List<OnePlugin> FinalPluginList = new List<OnePlugin>();
 
         public override void OnEnabled()
         {
             shouldSendDebug = Config.IsDebugEnabled;
             updatedplugins = 0;
 
-            GetPluginListURL();
+            PluginList ApiPluginList = GetPluginListByURL("https://plugins.exiled.host/api/plugins?apikey=" + Config.ApiKey);
+            foreach (ApiPluginInfo pluginInfo in ApiPluginList.success)
+            {
+                foreach (File file in pluginInfo.files)
+                {
+                    OnePlugin newPlugin = new OnePlugin
+                    {
+                        name = file.file_name,
+                        latest_version = pluginInfo.latest_version,
+                        id = pluginInfo.id
+                    };
+                    Log.Info(file.file_name + "." + file.file_extension + " - added + " + pluginInfo.latest_version);
+                    FinalPluginList.Add(newPlugin);
+                }
+            }
 
             MEC.Timing.CallDelayed(3, () => 
             { 
@@ -49,19 +64,6 @@ namespace JesusQCsAutoUpdater
             base.OnDisabled();
         }
 
-        public void GetPluginListURL()
-        {
-            const string url = "https://jesus-qc.es/autoupdaterapi.json";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
-            var webResponse = request.GetResponse();
-            var webStream = webResponse.GetResponseStream();
-            var responseReader = new StreamReader(webStream);
-            var response = responseReader.ReadToEnd();
-            pluginList = Utf8Json.JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(response);
-            responseReader.Close();
-        }
-
         public void CheckForUpdates()
         {
             try
@@ -75,15 +77,8 @@ namespace JesusQCsAutoUpdater
                     }
                     else
                     {
-                        if (pluginList.ContainsKey(plugin.Name))
-                        {
-                            CheckVersion(plugin);
-                        }
-                        else
-                        {
-                            Log.Debug(plugin.Name + " is not registered in the plugin list, skipping it.", shouldSendDebug);
-                            updatedplugins++;
-                        }
+                        CheckVersion(plugin);
+                        updatedplugins++;
                     }
                     AllPluginsUpdated();
                 }
@@ -97,52 +92,126 @@ namespace JesusQCsAutoUpdater
         public void CheckVersion(IPlugin<IConfig> plugin)
         {
             Log.Debug("Checking the version of " + plugin.Name, shouldSendDebug);
-            try
-            {
-                Dictionary<string, string> versionList = pluginList[plugin.Name];
-                Version listedVersion = new Version(versionList.Keys.FirstOrDefault());
-                if (plugin.Version < listedVersion)
-                {
-                    Log.Warn(plugin.Name + " [" + plugin.Version + "] is outdated. Latest version: [" + listedVersion + "]. Updating it...");
 
-                    try
-                    {
-                        File.Delete(plugin.GetPath());
-                        using (var client = new WebClient())
-                        {
-                            client.DownloadFile(versionList.Values.FirstOrDefault(), plugin.GetPath());
-                        }
-                        Log.Info(plugin.Name + " was updated successfully!");
-                        updatedplugins++;
-                    }
-                    catch(Exception e)
-                    {
-                        Log.Error(plugin.Name + " couldn't be updated " + e);
-                    }
-                }
-                else if (plugin.Version == listedVersion)
-                {
-                    Log.Debug(plugin.Name + " " + plugin.Version + " is updated", shouldSendDebug);
-                    updatedplugins++;
-                }
-                else
-                {
-                    Log.Debug(plugin.Name + " " + plugin.Version + " is updated", shouldSendDebug);
-                    updatedplugins++;
-                }
-            }
-            catch(Exception e)
+            foreach (OnePlugin pluginInfo in FinalPluginList.Where(p => p.name == plugin.Assembly.GetName().Name))
             {
-                Log.Error(e);
+                if (plugin.Version < new Version(pluginInfo.latest_version))
+                {
+                    Log.Warn(plugin.Name + " [" + plugin.Version + "] is outdated. Latest version: [" + pluginInfo.latest_version + "]. Updating it...");
+                    using (var client = new WebClient())
+                    {
+                        System.IO.File.Delete(plugin.GetPath());
+                        client.DownloadFile(GetLastestVersionByID(pluginInfo.id).success.latest_download_link, plugin.GetPath());
+                        Log.Info(plugin.Name + " was updated successfully!");
+                    }
+                }
+                else if (plugin.Version >= new Version(pluginInfo.latest_version))
+                {
+                    Log.Debug(plugin.Name + " " + plugin.Version + " is updated", shouldSendDebug);
+                }
+                return;
             }
         }
 
         public void AllPluginsUpdated()
         {
-            if (updatedplugins == Exiled.Loader.Loader.Plugins.Count)
+            if (updatedplugins == Loader.Plugins.Count)
             {
                 Log.Warn("All plugins are updated!");
             }
+        }
+
+        public PluginList GetPluginListByURL(string url)
+        {
+            using (var client = new WebClient())
+            {
+                string response = client.DownloadString(url);
+                PluginList deserializedClass = Utf8Json.JsonSerializer.Deserialize<PluginList>(response);
+                return deserializedClass;
+            }
+        }
+
+        public FinalPluginInfo GetLastestVersionByID(int id)
+        {
+            using (var client = new WebClient())
+            {
+                string response = client.DownloadString(new Uri("https://plugins.exiled.host/api/plugins/" + id));
+                FinalPluginInfo deserializedClass = Utf8Json.JsonSerializer.Deserialize<FinalPluginInfo>(response);
+                return deserializedClass;
+            }
+        }
+
+        public class OnePlugin
+        {
+            public string name { get; set; }
+            public string latest_version { get; set; }
+            public int id { get; set; }
+        }
+
+        public class Success
+        {
+            public string latest_version { get; set; }
+            public string latest_exiled_version { get; set; }
+            public string latest_download_link { get; set; }
+        }
+
+        public class FinalPluginInfo
+        {
+            public Success success { get; set; }
+        }
+
+        public class User
+        {
+            public object id { get; set; }
+            public string nickname { get; set; }
+        }
+
+        public class File
+        {
+            public int file_id { get; set; }
+            public int plugin_id { get; set; }
+            public int type { get; set; }
+            public string file_name { get; set; }
+            public string file_extension { get; set; }
+            public int file_size { get; set; }
+            public string upload_time { get; set; }
+            public string exiled_version { get; set; }
+            public string version { get; set; }
+            public int downloads_count { get; set; }
+            public string changelog { get; set; }
+        }
+
+        public class Categoryobj
+        {
+            public int id { get; set; }
+            public string category_name { get; set; }
+            public string category_color { get; set; }
+        }
+
+        public class ApiPluginInfo
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string image_url { get; set; }
+            public string small_description { get; set; }
+            public string description { get; set; }
+            public string wiki_url { get; set; }
+            public string issues_url { get; set; }
+            public string source_url { get; set; }
+            public int latest_file_id { get; set; }
+            public string latest_exiled_version { get; set; }
+            public string latest_version { get; set; }
+            public string last_update { get; set; }
+            public string creation_date { get; set; }
+            public int downloads_count { get; set; }
+            public User user { get; set; }
+            public List<File> files { get; set; }
+            public Categoryobj categoryobj { get; set; }
+        }
+
+        public class PluginList
+        {
+            public List<ApiPluginInfo> success { get; set; }
         }
     }
 }
--- Config.cs
+++ Config.cs
@@ -12,6 +12,9 @@ namespace JesusQCsAutoUpdater
         [Description("Is debug enabled?")]
         public bool IsDebugEnabled { get; set; } = false;
 
+        [Description("Official Web ApiKey, get yours in plugins.exiled.host")]
+        public string ApiKey { get; set; } = "00000000000000000000000000000000";
+        
         [Description("Plugin blacklist, this plugins wont be updated automatically (Use the name of the plugin in the config [Prefix]).")]
         public List<string> pluginBlacklist { get; set; } = new List<string>
         {
